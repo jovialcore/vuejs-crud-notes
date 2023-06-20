ef5c0fd3a0a97c43ea20a2b847ef8910cd1d42a0
jovialcore <jollydosky@gmail.com>
2023-06-20 14:15:00 +0100

    show added

--- src/components/ShowNote.vue
+++ src/components/ShowNote.vue
@@ -1,11 +1,41 @@
 <template>
-    <p>Show anote </p>
+    <router-link class="btn btn-secondary" role="button" :to="{ name: 'landing' }"><i class="fa fa-arrow-left"></i> back
+    </router-link>
+
+    <div class="card my-3">
+        <div class="card-body">
+            <h5 class="card-title">Note title : {{ title }} </h5>
+            <p class="card-text"> Description: {{ text }}</p>
+        </div>
+    </div>
 </template>
 
 
 <script>
+
+import { ref, onMounted } from "vue";
+
+import { useRoute } from 'vue-router'
 export default {
     name: 'ShowNote',
+
+
+
+    setup() {
+        const title = ref('')
+        const text = ref('')
+        const note = ref([])
+        const id = useRoute().params.id
+
+        onMounted(() => {
+            note.value = JSON.parse(localStorage.getItem('notes')).find(item => item.id == id)
+            title.value = note.value.title
+            text.value = note.value.text
+
+        })
+
+        return { title, text, note }
+    }
 
 }
 </script>
